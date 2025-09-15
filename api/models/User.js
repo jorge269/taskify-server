25ab0ed568807e6b6d29b4f6d689c5a089e6dcc7
Gabriela Guzmán <136855829+gabrielaguzmn@users.noreply.github.com>
2025-09-15 15:58:24 -0500

    modify user schema

--- api/models/User.js
+++ api/models/User.js
@@ -7,6 +7,8 @@ const UserSchema = new mongoose.Schema(
     age: { type: Number, required: true},
     password: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    resetPasswordToken: {type: String},
+    resetPasswordExpires: {type: String}
   },
   { timestamps: true }
 );
